f5055a34b1327bafe247eba05d8c20c6f7c0892a
ZahrahNaveed <zahrahnaveed.1911@gmail.com>
2025-08-11 17:00:22 +0500

    Footer section

--- src/components/Billing.jsx
+++ src/components/Billing.jsx
@@ -7,6 +7,7 @@ const Billing = () => (
     <div className={layout.sectionImgReverse}>
       <img src={bill} alt="billing" className="w-[100%] h-[100%] relative z-[5]" />
       <div className="absolute z-[3] -left-1/2 top-0 w-[50%] h-[50%] rounded-full white__gradient" />
+      <div className="absolute z-[2] -left-1/2 bottom-0 w-[50%] h-[50%] pink__gradient" />
     </div>
 
     <div className={layout.sectionInfo}>
